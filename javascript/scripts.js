a0faf1ecef59eaeff9e3f16f4dc40ad56816c495
skouroufexis <skouroufexis@gmail.com>
2020-03-09 11:42:09 +0200

    added IIFE
    - enclosed array creation (for 'repository' array), retrieval and updating in an IIFE
    -assigned the IIFE to a variable ('pokemonRepository')
    -iterate through the repository array, by gaining access to it through 'pokemonRepository'

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,48 +1,40 @@
-var repository = [];
 
 
 
+var pokemonRepository = (function(){
+    
+    
+    var repository = [];
+    var charmeleon = {name:'charmeleon',height:1.1,types:['ground','rock','water']};
+    var metapod = {name:'Metapod',height:0.7,types:['rock','fire','gas']};
+    var bulbasaur = {name:'Bulbasaur',height:0.7,types:['fire','ice','electric']};
+    var squirtle = {name:'Squirtle',height:0.5,types:['grass','electric','steel']};
+    
+    repository[0]= charmeleon;
+    repository[1]= metapod ;
+    repository[2]= bulbasaur;
+    repository[3]= squirtle;
+    
+    
+    function getAll(){
+        return repository;
+    }
 
+    function add(item){
+        if(typeof item==='object' && Object.keys(item)[0]==='name' && Object.keys(item)[1]==='height' && Object.keys(item)[2]==='types')
+            {
+                repository.push(item);        
+                    
+            }
+    } 
+    
+    return {getAll:getAll, add:add};   
+    
+})();
 
-var charmeleon = {name:'charmeleon',height:1.1,types:['ground','rock','water']};
-var metapod = {name:'Metapod',height:0.7,types:['rock','fire','gas']};
-var bulbasaur = {name:'Bulbasaur',height:0.7,types:['fire','ice','electric']};
-var squirtle = {name:'Squirtle',height:0.5,types:['grass','electric','steel']};
-
-
-repository[0]= charmeleon;
-repository[1]= metapod ;
-repository[2]= bulbasaur;
-repository[3]= squirtle;
-
-
-//for1(repository);
+var repository = pokemonRepository.getAll();
 for2(repository);
 
-//function for1(repository){
-//    
-//    var l = repository.length;
-//    var c;
-//    var t="";
-//    
-//    
-//    for(c=0;c<l;c++){
-//    
-//        if(repository[c].height>1)
-//                {
-//                    t='<div class="row">'+repository[c].name+' (height:'+repository[c].height+')'+'  <span class="span_highlight">Wow, that’s big!</span>'+'</div><br>';
-//                }
-//
-//        else
-//                {
-//                    t='<div class="row">'+repository[c].name+' (height:'+repository[c].height+')'+'</div><br>';
-//                }
-//
-//        document.write(t);
-//    
-//    }
-//    
-//}
 
 
 
